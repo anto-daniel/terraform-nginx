60b91b22ce5a2e9cacee789fc4bddb39287c0a98
Anto Daniel <anto.daniel@gmail.com>
2025-07-09 20:22:05 +0530

    Update content in github repo

--- terraform-code/main.tf
+++ terraform-code/main.tf
@@ -4,3 +4,11 @@ resource "github_repository" "mtc_repo" {
 	description = "Code for MTC"
 	auto_init = true
 }
+
+resource "github_repository_file" "readme" {
+	repository = github_repository.mtc_repo.name
+	content = "# This repository is for infra developers"
+	file = "README.md"
+	branch = "main"
+	overwrite_on_create = true
+}
