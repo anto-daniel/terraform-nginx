fed353a41de62d96157e5f21a077abf17a9bcb49
Anto Daniel <anto.daniel@gmail.com>
2025-07-10 15:18:15 +0530

    Update main.tf with another github file

--- terraform-code/main.tf
+++ terraform-code/main.tf
@@ -1,14 +1,22 @@
 resource "github_repository" "mtc_repo" {
-	name = "mtc-repo"
-	visibility = "private"
-	description = "Code for MTC"
-	auto_init = true
+  name        = "mtc-repo"
+  visibility  = "private"
+  description = "Code for MTC"
+  auto_init   = true
 }
 
 resource "github_repository_file" "readme" {
-	repository = github_repository.mtc_repo.name
-	content = "# This repository is for infra developers"
-	file = "README.md"
-	branch = "main"
-	overwrite_on_create = true
+  repository          = github_repository.mtc_repo.name
+  content             = "# This repository is for infra developers"
+  file                = "README.md"
+  branch              = "main"
+  overwrite_on_create = true
+}
+
+resource "github_repository_file" "index" {
+  repository          = github_repository.mtc_repo.name
+  content             = "Hello World"
+  file                = "index.html"
+  branch              = "main"
+  overwrite_on_create = true
 }
