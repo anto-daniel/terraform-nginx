5c10956ce7c236f1c8d41cfab7125244d612cc04
Anto Daniel <anto.daniel@gmail.com>
2025-07-21 23:08:34 +0530

    ignore varsource

--- terraform-code/variables.tf
+++ terraform-code/variables.tf
@@ -2,10 +2,15 @@ variable "repo_count" {
   type        = number
   description = "Number of repositories"
   default     = 1
+
+  validation {
+    condition     = var.repo_count < 5
+    error_message = "The repo_count must be less than 5."
+  }
 }
 
-variable "varsource" {
-  type        = string
-  description = "Source of the variable file"
-  default     = "variables.tf"
-}
+#variable "varsource" {
+#  type        = string
+#  description = "Source of the variable file"
+#  default     = "variables.tf"
+#}
