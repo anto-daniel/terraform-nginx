7e2136131d18e511d5af8330092ae3ec751ad12c
Anto Daniel <anto.daniel@gmail.com>
2025-07-22 17:05:17 +0530

    Removing other functions for now

--- terraform-code/main.tf
+++ terraform-code/main.tf
@@ -1,7 +1,7 @@
-resource "random_id" "random" {
-  byte_length = 2
-  count       = var.repo_count
-}
+#resource "random_id" "random" {
+#  byte_length = 2
+#  count       = var.repo_count
+#}
 
 resource "github_repository" "mtc_repo" {
   for_each    = toset(["dev", "prod"])
@@ -11,24 +11,24 @@ resource "github_repository" "mtc_repo" {
   auto_init   = true
 }
 
-resource "github_repository_file" "readme" {
-  count               = var.repo_count
-  repository          = github_repository.mtc_repo[count.index].name
-  content             = "# This repository is for infra developers"
-  file                = "README.md"
-  branch              = "main"
-  overwrite_on_create = true
-}
-
-resource "github_repository_file" "index" {
-  count               = var.repo_count
-  repository          = github_repository.mtc_repo[count.index].name
-  content             = "Hello World"
-  file                = "index.html"
-  branch              = "main"
-  overwrite_on_create = true
-}
-
+#resource "github_repository_file" "readme" {
+#  count               = var.repo_count
+#  repository          = github_repository.mtc_repo[count.index].name
+#  content             = "# This repository is for infra developers"
+#  file                = "README.md"
+#  branch              = "main"
+#  overwrite_on_create = true
+#}
+#
+#resource "github_repository_file" "index" {
+#  count               = var.repo_count
+#  repository          = github_repository.mtc_repo[count.index].name
+#  content             = "Hello World"
+#  file                = "index.html"
+#  branch              = "main"
+#  overwrite_on_create = true
+#}
+#
 output "clone-urls" {
 
   value       = { for i in github_repository.mtc_repo[*] : i.name => i.http_clone_url }
