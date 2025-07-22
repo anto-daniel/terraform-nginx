cf572cb175d78b703f461117ae98007cdb3a55bf
Anto Daniel <anto.daniel@gmail.com>
2025-07-22 22:57:05 +0530

    adding ssh clone url

--- terraform-code/main.tf
+++ terraform-code/main.tf
@@ -31,7 +31,7 @@ resource "github_repository" "mtc_repo" {
 #
 output "clone-urls" {
 
-  value       = { for i in github_repository.mtc_repo : i.name => i.http_clone_url }
+  value       = { for i in github_repository.mtc_repo : i.name => [i.ssh_clone_url, i.http_clone_url] }
   description = "Repository Name and Clone URLs"
   sensitive   = false
 }
